c7b49834e574cd022b653dfb492992fd89f8e904
Rifqi39 <150407936+Rifqi39@users.noreply.github.com>
2024-02-26 20:05:30 +0700

    Update kategori.php

--- kategori.php
+++ kategori.php
@@ -199,7 +199,7 @@ if ($login == 1) {
     </div>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Kategori</h1>
+        <h1 class="h2">Tambah Kategori</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
          
         </div>
